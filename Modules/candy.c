bf6c0ea219b062be95b0de935e55c191013f5f51
Ellu <31679332+ElluIFX@users.noreply.github.com>
2021-12-25 20:43:49 +0800

    规范化注释

--- Modules/candy.c
+++ Modules/candy.c
@@ -10,10 +10,23 @@
 
 #include "candy.h"
 
+/**
+ * @brief Linear mapping input to the specified range
+ * @param  x                input value
+ * @param  in_min           input range min
+ * @param  in_max           input range max
+ * @param  out_min          output range min
+ * @param  out_max          output range max
+ * @retval Mapping result
+ */
 float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+/**
+ * @brief Delay_us, usint a timer prescale to 1Mhz, reloadRegister = 1
+ * @param  us               delay time in us
+ */
 void delay_us(uint16_t us) {
   uint16_t differ = 0xffff - us - 5;
   HAL_TIM_Base_Start(&_DELAY_TIM);
@@ -25,7 +38,8 @@ void delay_us(uint16_t us) {
 }
 
 /**
- * @brief Send data bits to WS2812, can be used only when MCU's SYSCLK is 168MHz.
+ * @brief Send data bits to WS2812, can be used only when MCU's SYSCLK is
+ * 168MHz.
  * @param  data             uint8_t color array in G R B order, total length is
  * 3 * number of LEDs, the first LED in left.
  * @param  len              the number of LEDs
--- Modules/candy.h
+++ Modules/candy.h
@@ -10,17 +10,27 @@
 
 #ifndef CANDY_H
 #define CANDY_H
+
 #include <main.h>
 
 #include "tim.h"
 
+// HMI screen
+
 #define S_END_BIT "\xff\xff\xff"
 #define screen(x, ...) printft(&huart2, x, __VA_ARGS__)
+
+// delay_us
+
 #define _DELAY_TIM htim6
+void delay_us(uint16_t us);
+
+// fmap
 
 float fmap(float x, float in_min, float in_max, float out_min, float out_max);
-void delay_us(uint16_t us);
-void WS2812_SendBit(uint8_t* data, uint16_t le);
+
+// WS2812
+
 #define WS2812_PIN WS2812_GPIO_Port, WS2812_Pin
 #define __DELAY_350NS for (uint8_t i = 0; i < 8; i++)
 #define __DELAY_600NS for (uint8_t i = 0; i < 13; i++)
@@ -37,5 +47,7 @@ void WS2812_SendBit(uint8_t* data, uint16_t le);
   HAL_GPIO_WritePin(WS2812_PIN, GPIO_PIN_RESET); \
   __DELAY_800NS
 #define __2812_RESET delay_us(60)
-#define __SET_2812(r,g,b) WS2812_SendBit((uint8_t[]){g,r,b}, 1) 
+#define __SET_2812_RGB(r, g, b) WS2812_SendBit((uint8_t[]){g, r, b}, 1)
+void WS2812_SendBit(uint8_t* data, uint16_t le);
+
 #endif
